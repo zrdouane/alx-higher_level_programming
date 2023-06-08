8f228708f367693e82fc370737cbd856200d044d
zrdouane <ramirdouane@gmail.com>
2023-06-08 08:22:12 -0700

    write a function that print numbers of arg entred

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    longueur = len(argv)
+    if longueur == 1:
+        print("0 arguments.")
+    elif longueur == 2:
+        print("1 argument:")
+    else:
+        print("{} argument:.".format(longueur - 1))
+    for index in range(1, longueur):
+        print("{}: {}".format(index, argv[index]))
