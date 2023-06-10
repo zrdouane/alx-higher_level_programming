9523dd518b8197a934ebff59ef7647f35ab394b0
zrdouane <ramirdouane@gmail.com>
2023-06-10 05:21:12 -0700

    funcion that print max INT from a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    max_num = my_list[0]
-    for num in my_list:
-        if num > max_num:
-            max_num = num
+    if len(my_list) == 0:
+        return (None)
 
-    return max_num
+    mx = my_list[0]
+    for index in range(len(my_list)):
+        if my_list[index] > mx:
+            mx = my_list[index]
 
+    return (mx)
