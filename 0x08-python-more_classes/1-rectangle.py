e0d0bd4bf83c958f768a8ce1f1cd957eb33a112e
zrdouane <ramirdouane@gmail.com>
2023-07-03 05:28:07 -0700

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Defines a class Rectangle
 """
@@ -6,35 +7,36 @@ Defines a class Rectangle
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-        """Initializing the rectangle by setting the object with width and height"""
-        self._height = height
-        self._width = width
+        """initializing the rectangle by
+        setting the objet with width and height
+        """
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """Getter for the private instance attribute width"""
-        return self._width
+        """getter for the private instance attribute width"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for the private instance attribute width"""
-        if not isinstance(value, int):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Getter for the private instance attribute height"""
-        return self._height
+        """getter for the private instance attribute height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for the private instance attribute height"""
-        if not isinstance(value, int):
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
-
+        self.__height = value
