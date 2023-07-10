2849322d659c8b55b4232c2cedaccea9f491eae2
zrdouane <ramirdouane@gmail.com>
2023-07-10 03:45:47 -0700

    function that returns the list of available attributes and methods of an object && add readme.md

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module for lookup function
+"""
+
+def lookup(obj):
+    """
+    Args:
+        obj: initial
+        Returns: a list of attributes
+    """
+    return dir(obj)
