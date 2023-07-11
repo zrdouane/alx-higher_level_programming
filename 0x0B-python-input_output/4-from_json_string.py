9c5cb758508a5a5da185bbf29146363c414fb460
zrdouane <ramirdouane@gmail.com>
2023-07-11 03:36:03 -0700

    function that returns an object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -10,6 +10,9 @@ import json
 def from_json_string(my_str):
     """
     returns an object (Python data structure) represented by a JSON string
+
+    Arguments:
+        my_str {str} -- [string to be converted]
     """
     return json.loads(my_str)
 
