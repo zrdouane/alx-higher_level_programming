46ff73881532edae0ec0eb88805720c27697c592
zrdouane <ramirdouane@gmail.com>
2023-09-11 10:37:04 +0100

    task 7 done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-
-if (arg === undefined || isNaN(arg) || arg < 1) {
+const argv = process.argv;
+const x = parseInt(argv[2]);
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  const x = parseInt(arg);
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
