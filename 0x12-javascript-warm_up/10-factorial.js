7a367854aaf4bbd4578d18eba15ff118740c466c
zrdouane <ramirdouane@gmail.com>
2023-09-11 10:43:24 +0100

    task 10 done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
-
 function factorial (n) {
-  if (isNaN(n)) {
-    return 1;
-  }
-
-  n = parseInt(n);
-
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-
-  return n * factorial(n - 1);
+  if (isNaN(n) || n < 2) return 1;
+  else return n * factorial(n - 1);
 }
-
-const arg = process.argv[2];
-const result = factorial(arg);
-
-console.log(result);
+console.log(factorial(process.argv[2]));
