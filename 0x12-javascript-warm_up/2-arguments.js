ee9c62179b90f76ba8a57c8cc9e1a0add6b76021
zrdouane <ramirdouane@gmail.com>
2023-09-11 10:16:00 +0100

    task2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/local/bin/node
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
