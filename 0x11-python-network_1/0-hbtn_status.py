93fc7a0b29c48cff70c2590bc982a13ceca8e9fb
zrdouane <ramirdouane@gmail.com>
2023-09-29 11:18:35 +0100

    fix style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = "https://intranet.hbtn.io/status"
     with urlopen(url) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
