6dfbb821ee7fd2a649cd7ce181fb4ba17c22e591
zrdouane <ramirdouane@gmail.com>
2023-10-24 11:49:00 +0100

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /* script that reads and prints the content of a file. */
 
-const fs = require('fs')
-const fn = process.argv[2]
+const fs = require('fs');
+const fn = process.argv[2];
 fs.readFile(fn, 'utf8', (err, dt) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(dt.toString())
-	}
-})
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(dt.toString());
+  }
+});
